cae1766de17555a231e094b7ea3a41ba08d73bac
Ainslie Hsu <ainslie.hsu@gmail.com>
2018-08-25 19:44:27 -0700

    Add author and update retweet param variable name

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const config = require('./config.js');
 const client = new Twitter(config);
 
 let searchParams = {
-  q: 'baby elephants',
+  q: 'fake news',
   count: 3,
   lang: 'en',
 };
@@ -28,13 +28,12 @@ client.get('search/tweets', searchParams, function(err, data, response) {
     })();
 
     // data.statuses.forEach(element => {
-    //   let id = {
+    //   let retweetParams = {
     //     id: element.id_str,
     //   }
-    //   client.post('statuses/retweet', id, function(err, data, response) {
-    //     console.log(id.id)
+    //   client.post('statuses/retweet', retweetParams, function(err, data, response) {
     //     if (!err) {
-    //       console.log(`Retweeted https://api.twitter.com/1.1/statuses/retweet/${id.id}.json`)
+    //       console.log(`Retweeted https://api.twitter.com/1.1/statuses/retweet/${retweetParams.id}.json`)
     //     } else {
     //       console.log(err);
     //     }
